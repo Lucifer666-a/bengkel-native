57fa60881d2b6d3ceffd8e9751bca4e6072128f0
Frnndo_H.N <fernandohasiholan2956@gmail.com>
2026-06-18 00:46:47 +0700

    error

--- config/koneksi.php
+++ config/koneksi.php
@@ -9,4 +9,10 @@ $conn = new mysqli($host, $username, $password, $database);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+global $conn; 
+    
+    $eksekusi = mysqli_query($conn, $sql) or die("<b>Database Error:</b> " . mysqli_error($conn));
+    
+    return $eksekusi;
 ?>
